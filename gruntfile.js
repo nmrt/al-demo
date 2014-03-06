0e662595484f789a3ec0a4aae48fe1a4f5bb96c1
Artem Nyzhnyk <artem@sichnevyj.com>
2014-03-06 21:52:20 +0200

    Not forcing Grunt, breaking on warnings.

--- gruntfile.js
+++ gruntfile.js
@@ -84,7 +84,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-env');
 
     //Making grunt default to force in order not to break the project.
-    grunt.option('force', true);
+    //grunt.option('force', true);
 
     //Default task(s).
     grunt.registerTask('default', ['jshint', 'concurrent']);
