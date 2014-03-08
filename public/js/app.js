77a895186fc30378d2e580153378383d86500efe
Artem Nyzhnyk <artem@sichnevyj.com>
2014-03-08 18:07:41 +0200

    Article wipe out

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'mean.system', 'mean.articles']);
+angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'mean.system']);
 
 angular.module('mean.system', []);
-angular.module('mean.articles', []);
--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -3,13 +3,7 @@
 angular.module('mean.system').controller('HeaderController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
 
-    $scope.menu = [{
-        'title': 'Articles',
-        'link': 'articles'
-    }, {
-        'title': 'Create New Article',
-        'link': 'articles/create'
-    }];
+    $scope.menu = [];
     
     $scope.isCollapsed = false;
 }]);
