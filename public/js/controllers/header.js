86ec3b08faaa2cdef9db1adc2dbfb147b72d9bc0
Artem Nyzhnyk <artem@sichnevyj.com>
2014-03-18 22:53:43 +0200

    Front end menu (in the header)

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -3,7 +3,10 @@
 angular.module('mean.system').controller('HeaderController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
 
-    $scope.menu = [];
+    $scope.menu = [
+        {title: 'List orders', state: 'orders/list'},
+        {title: 'Create order', state: 'orders/create'}
+    ];
     
     $scope.isCollapsed = false;
 }]);
